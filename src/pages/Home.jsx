5818eb6f5e1b8541db871520e1bdf6684343e7f6
gtsasuk <tsasiuk.yevhenii.clg@chnu.edu.ua>
2025-04-04 10:44:34 +0300

    public

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../сss/Home.css"
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1>Traffic Lights React-project</h1>
+      <p>Select one of the traffic light display options above.</p>
+    </div>
+  );
+};
+
+export default Home;
