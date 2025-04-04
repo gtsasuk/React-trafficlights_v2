ca559c751294311a1f8e241cf854655360f12f95
gtsasuk <tsasiuk.yevhenii.clg@chnu.edu.ua>
2025-04-04 11:08:54 +0300

    add test 3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     <div className="home-container">
       <h1>Traffic Lights React-project</h1>
       <p>Select one of the traffic light display options above.</p>
-      <p>Test redeploy 2</p>
+      <p>Test redeploy 3</p>
     </div>
   );
 };
